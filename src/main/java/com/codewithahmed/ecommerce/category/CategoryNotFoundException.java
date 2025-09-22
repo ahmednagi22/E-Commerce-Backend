591e6701c67d95c91cd5a9a531691e0e88b3c454
Ahmed Abdelmongy <ahmednagi318@gmail.com>
2025-09-22 20:03:15 +0300

    create exception for each service(product,category)

---
+++ src/main/java/com/codewithahmed/ecommerce/category/CategoryNotFoundException.java
@@ -0,0 +1,14 @@
+package com.codewithahmed.ecommerce.category;
+
+public class CategoryNotFoundException extends RuntimeException {
+
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+    public CategoryNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public CategoryNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/com/codewithahmed/ecommerce/product/ProductNotFoundException.java
@@ -0,0 +1,14 @@
+package com.codewithahmed.ecommerce.product;
+
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+    public ProductNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public ProductNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
